378145e27ef7cd2f727b922a3aa76adc999dbf7a
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-08 10:50:05 -0500

    Update solution.java

--- Tree/LC94.BinaryTreeInorderTraversal/solution.java
+++ Tree/LC94.BinaryTreeInorderTraversal/solution.java
@@ -8,6 +8,8 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//recursion
+/***
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         
@@ -29,5 +31,25 @@ class Solution {
             }
         }
         
+    }
+}*/
+//iteration
+class Solution{
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List < Integer> res = new ArrayList<>();
+        Stack< TreeNode > stack = new Stack<>();
+        TreeNode curr = root;
+        
+        while( curr != null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
     }
 }
