173d58f700a7211e73691d841d3850a48fdb2c73
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-08 20:08:32 -0500

    Update solution.java

--- Tree/100. Same Tree/solution.java
+++ Tree/100. Same Tree/solution.java
@@ -8,7 +8,9 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+//recursion
+/***
+ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
         if( (p == null && q != null) || (p != null && q == null)){
@@ -19,4 +21,40 @@ class Solution {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
+}
+*/
+
+//iteration
+class Solution{
+public boolean isSameTree(TreeNode p, TreeNode q) {
+    // iteration method
+    if (p == null && q == null) return true;
+    if (p == null && q != null || p != null && q == null) return false;
+    Stack<TreeNode> stackP = new Stack<>();
+    Stack<TreeNode> stackQ = new Stack<>();
+    stackP.add(p);
+    stackQ.add(q);
+    while (!stackP.isEmpty() && !stackQ.isEmpty()) {
+        TreeNode tmpP = stackP.pop();
+        TreeNode tmpQ = stackQ.pop();
+        if (tmpP.val != tmpQ.val) return false;
+        if (tmpP.left != null && tmpQ.left != null) {
+            stackP.push(tmpP.left);
+            stackQ.push(tmpQ.left);
+        } else if (tmpP.left == null && tmpQ.left == null) {
+        } else {
+            return false;
+        }
+        
+        if (tmpP.right != null && tmpQ.right != null) {
+            stackP.push(tmpP.right);
+            stackQ.push(tmpQ.right);
+        } else if (tmpP.right == null && tmpQ.right == null) {
+        } else {
+            return false;
+        }
+    }
+    if (!stackP.isEmpty() || !stackQ.isEmpty()) return false;
+    return true;
+}
 }
