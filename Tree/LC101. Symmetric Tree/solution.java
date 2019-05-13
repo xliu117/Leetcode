8c18dbe0972b5f504281c95f382feb1ee191dd2a
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-13 15:15:37 -0500

    Create solution.java

---
+++ Tree/LC101. Symmetric Tree/solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/***
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        //if(root == null) return true;
+        return symmetric(root, root);
+    }
+    
+    public boolean symmetric(TreeNode root1, TreeNode root2){
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        return(root1.val == root2.val) && symmetric(root1.right,root2.left)&&symmetric(root2.left,root1.right);
+
+        
+    }
+}*/
+
+
+class Solution{
+    public boolean isSymmetric(TreeNode root){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null) return false;
+            if(t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+}
