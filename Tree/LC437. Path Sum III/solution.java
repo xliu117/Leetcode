0aabc2ae9745e3cda099af99d24e63fc2a27c6d2
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-19 11:45:01 -0500

    Create solution.java

---
+++ Tree/LC437. Path Sum III/solution.java
@@ -0,0 +1,13 @@
+//double recursion
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) return 0;
+        return numberofPaths(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+    
+    private int numberofPaths(TreeNode root, int sum){
+        if(root == null) return 0;
+        //leftsum = leftsum - root.val;
+        return (root.val == sum ? 1 : 0) + numberofPaths(root.left, sum - root.val) + numberofPaths(root.right, sum - root.val);
+    }
+}
