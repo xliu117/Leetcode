fcf3e61cc0f16d8f32ac7651bb9dcf24177b271a
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-19 16:16:59 -0500

    Update solution.java

--- Tree/LC437. Path Sum III/solution.java
+++ Tree/LC437. Path Sum III/solution.java
@@ -18,3 +18,24 @@ class Solution {
 //Solution2:Running Prefix sum
 //Idea similar to LC560
 
+class Solution{
+ public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        return helper(root, 0, sum, preSum);
+    }
+    
+    public int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+        
+        currSum += root.val;
+        int res = preSum.getOrDefault(currSum - target, 0);
+        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+        
+        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+        return res;
+    }
+}
