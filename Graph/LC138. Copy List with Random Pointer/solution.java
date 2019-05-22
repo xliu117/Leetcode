12cb2af7e7b32073bf8dcfe82628fc7cd3ee7948
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-21 22:59:40 -0500

    Update solution.java

--- Graph/LC138. Copy List with Random Pointer/solution.java
+++ Graph/LC138. Copy List with Random Pointer/solution.java
@@ -1,1 +1,50 @@
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+		
+        //1. duplicate each node
+        Node node = head;
+        while (node != null){
+            Node newNode = new Node();
+            newNode.val = node.val;
+            newNode.next = node.next;
+            node.next = newNode;
+            node = newNode.next;
+        }
+        
+		//2. set random pointer
+        node = head;
+        while (node != null){
+            node.next.random = node.random == null ? null : node.random.next;
+            node = node.next.next;
+        }
+        
+		//3. build new list
+        Node newHead = head.next;
+        node = newHead;
+        while (head != null){
+            head.next = head.next.next;
+            head = head.next;
+            node.next = head == null ? null : head.next; 
+            node = node.next;
+        }
+        return newHead;
+    }
+}
