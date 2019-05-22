ef3811379e1992e96dd52a355f4a0a52f2046015
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-22 12:36:54 -0500

    Create solution.java

---
+++ BST/LC700. Search in a Binary Search Tree/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/***
+//recursion
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return root;
+        if(root.val == val) return root;
+        else{
+            return (val < root.val)? searchBST(root.left, val) : searchBST(root.right, val);
+        }
+        
+        
+    }
+}
+*/
+//iteration
+class Solution{
+public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && root.val != val){
+            root = val<root.val? root.left:root.right;
+        }
+        return root;
+    }
+}
