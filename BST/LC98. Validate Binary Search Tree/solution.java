0f9123d5e102f59c6114f1d355d5b3df1e9ff0dc
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-22 11:17:12 -0500

    Create solution.java

---
+++ BST/LC98. Validate Binary Search Tree/solution.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/***
+class Solution {
+    public boolean helper(TreeNode node, Integer lower, Integer upper){
+        if(node == null) return true;
+        
+        int val = node.val;
+        if(lower != null && val <= lower) return false;
+        if(upper != null && val >= upper) return false;
+        
+        if(!helper(node.right, val, upper)) return false;
+        if(!helper(node.left, lower, val)) return false;
+        return true;
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+}
+*/
+class Solution{
+    LinkedList<TreeNode> stack = new LinkedList();
+    LinkedList<Integer> uppers = new LinkedList(), lowers = new LinkedList();
+    
+    public void update(TreeNode root, Integer lower, Integer upper){
+        stack.add(root);
+        lowers.add(lower);
+        uppers.add(upper);
+    }
+    
+    public boolean isValidBST(TreeNode root){
+        Integer lower = null, upper = null, val;
+        update(root, lower, upper);
+        
+        while(!stack.isEmpty()){
+            root = stack.poll();
+            lower = lowers.poll();
+            upper = uppers.poll();
+            
+            if(root == null) continue;
+            val = root.val;
+            if(lower != null && val <= lower)return false;
+            if(upper != null && val >= upper)return false;
+            update(root.right,val,upper);
+            update(root.left, lower, val);
+        }
+        return true;
+    }
+}
