1c6cb03335723c4c53d60b752830437939a677c3
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-05-24 12:15:36 -0500

    Create solution.java

---
+++ List/LC24. Swap Nodes in Pairs/solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode current = dummyHead;
+        while(current.next != null && current.next.next != null){
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummyHead.next;
+    }
+}
+
