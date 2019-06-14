d67fe5026cb1ab4ce96ea4b0b418e6e0aef9296d
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-14 11:45:46 -0500

    Update solution.java

--- Search/LC78. Subsets/solution.java
+++ Search/LC78. Subsets/solution.java
@@ -1,4 +1,4 @@
-//BackTracking:
+//1.BackTracking:
 //Time: O(n * 2^n)
 //Space: O(n/2 * 2^n) ~= O(n * 2^n)
 class Solution {
@@ -20,3 +20,18 @@ class Solution {
 
     }  
 }
+
+//2.
+public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int n : nums){
+            int size = result.size();
+            for(int i=0; i<size; i++){
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(n);
+                result.add(subset);
+            }
+        }
+        return result;
+    }
