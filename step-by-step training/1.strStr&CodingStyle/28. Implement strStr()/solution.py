2c3c4d9ceac3c127fd52df550b5a412b10e520a0
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-14 09:37:19 -0500

    Create solution.py

---
+++ step-by-step training/1.strStr&CodingStyle/28. Implement strStr()/solution.py
@@ -0,0 +1,21 @@
+//Brute Force
+//time complexity:O(N*M) space complexity:O(1)
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == "":
+            return 0
+        
+        for i in range(len(haystack) - len(needle) + 1):
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
+                if j == len(needle) - 1:
+                    return i
+        return -1
+        
