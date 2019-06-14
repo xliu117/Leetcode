6c0802228d8cf8dfd6195bac646d83a6fc8e1e6c
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-14 11:00:53 -0500

    Update solution.java

--- step-by-step training/1.strStr&CodingStyle/28. Implement strStr()/solution.java
+++ step-by-step training/1.strStr&CodingStyle/28. Implement strStr()/solution.java
@@ -130,3 +130,60 @@ class Solution{
 
     }
 }
+
+
+
+class Solution {
+  private int[] computeLPS(String str) { // computes Longest Prefix Suffix (LPS) array
+    int[] lps = new int[str.length()];
+    lps[0] = 0;
+    int i = 1; // always walks forward
+    int j = 0; // tracks prefix that matches suffix
+
+    while (i < str.length()) {
+      if (str.charAt(i) == str.charAt(j)) {
+        j++;
+        lps[i] = j;
+        i++;
+      } else { // mismatch
+        if (j == 0) { // go onto next character in string
+          lps[i] = 0;
+          i++;
+        } else { // backtrack j to check previous matching prefix
+          j = lps[j - 1];
+        }
+      }
+    }
+    return lps;
+  }
+
+  int strStr(String haystack, String needle) {
+      if (haystack == null || needle == null || haystack.length() < needle.length()) {
+          return -1;
+      } else if (needle.isEmpty()) {
+          return 0;
+      }
+
+      int[] lps = computeLPS(needle);
+      int i = 0;
+      int j = 0;
+
+      while (i < haystack.length()) {
+          if (needle.charAt(j) == haystack.charAt(i)) {
+              i++;
+              j++;
+              if (j == needle.length()) {
+                  return i - j; // match found. Return location of match
+              }
+          } else {
+              if (j == 0) {
+                  i++;
+              } else {
+                  j = lps[j - 1]; // backtrack j to check previous matching prefix
+              }
+          }
+      }
+
+      return -1; // did not find needle
+  }
+}
