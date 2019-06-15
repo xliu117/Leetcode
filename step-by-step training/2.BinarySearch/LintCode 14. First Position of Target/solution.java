5f0ee30b820031f7a87bb92928e7a1a933fa4d00
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-15 17:23:05 -0500

    Create solution.java

---
+++ step-by-step training/2.BinarySearch/LintCode 14. First Position of Target/solution.java
@@ -0,0 +1,23 @@
+public class Solution{
+    public int binarySearch(int[] nums, int target){
+         if (nums.length == 0 || nums == null) return -1;
+         
+         int start = 0;
+         int end = nums.length - 1;
+         
+         while(start + 1 < end){
+              int mid = start + (end - start)/2;
+              if(nums[mid]>=target){
+                    end = mid;
+                    }
+              else{
+                    start = mid;
+                    }
+                    
+           }
+           
+           
+           if(nums[start] == target) return start;
+           if(nums[end] == target) return end;
+           return -1;
+    
