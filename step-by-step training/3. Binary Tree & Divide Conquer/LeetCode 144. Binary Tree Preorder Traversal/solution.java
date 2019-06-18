64d0b3dfb3e40905db2f043473c32490e4b86782
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-18 16:16:51 -0500

    Update solution.java

--- step-by-step training/3. Binary Tree & Divide Conquer/LeetCode 144. Binary Tree Preorder Traversal/solution.java
+++ step-by-step training/3. Binary Tree & Divide Conquer/LeetCode 144. Binary Tree Preorder Traversal/solution.java
@@ -1,5 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/***
+//1.traverse recursion
 class Solution {
-    
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new LinkedList<>();
         traverse(root,res);      
@@ -11,5 +21,56 @@ class Solution {
         res.add(root.val);
         traverse(root.left,res);
         traverse(root.right,res);
+    }
+}
+
+
+//2.divide and conquer
+class Solution{
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        
+        
+        List<Integer> left = preorderTraversal(root.left);
+        List<Integer> right = preorderTraversal(root.right);
+        
+        result.add(root.val);
+        result.addAll(left);
+        result.addAll(right);
+        
+        return result;
+    }
+}
+*/
+
+
+//3.Iterative
+public class Solution{
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        
+        if(root == null) {
+             return result;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            
+            if(node.right != null){
+                stack.push(node.right);
+            }
+            
+            if(node.left != null){
+                stack.push(node.left);
+            }
+        }
+        return result;
     }
 }
