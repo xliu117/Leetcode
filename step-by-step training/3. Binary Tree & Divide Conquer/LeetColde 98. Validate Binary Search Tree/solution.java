e30cb6c3796645d12ce1f47d78b1996b3c860c39
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-18 13:16:13 -0500

    Create solution.java

---
+++ step-by-step training/3. Binary Tree & Divide Conquer/LeetColde 98. Validate Binary Search Tree/solution.java
@@ -0,0 +1,31 @@
+public class Solution{
+     private int lastVal = Integer.MIN_VALUE;
+     private boolean firstNode = true;
+     
+     public boolean isValidBST(TreeNode root){
+           if(root == null){
+                return true;
+                }
+                
+            if(!isValidBST(root.left)){
+                return false;
+                }
+                
+            if(!firstNode && root.val <= lastVal){
+                 return false;
+                 }
+                 
+            firstNode = false;
+            lastVal = root.val;
+            
+            if(!isValidBST(root.right)){
+                    return false;
+                    }
+                    
+                    
+            return true;
+     }
+}     
+           
+            
+       
