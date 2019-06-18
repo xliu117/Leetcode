db8827c96321b75f24f18ce844940d38683b97e6
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-18 12:49:33 -0500

    Update solution.java

--- step-by-step training/3. Binary Tree & Divide Conquer/LeetCode 110. Balanced Binary Tree/solution.java
+++ step-by-step training/3. Binary Tree & Divide Conquer/LeetCode 110. Balanced Binary Tree/solution.java
@@ -1,3 +1,5 @@
+/***
+//my solution
 //divide and conquer
 class Solution {
     private int gap = Integer.MIN_VALUE;
@@ -16,5 +18,42 @@ class Solution {
         int DepthOfRightSubtree = depth(root.right);
         gap = Math.max(gap,Math.abs(DepthOfLeftSubtree-DepthOfRightSubtree));
         return  Math.max(DepthOfLeftSubtree,DepthOfRightSubtree)+1;
+    }
+}
+*/
+
+
+//other solution
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        // write your code here
+        if(root == null){
+            return true;
+        }
+
+        if(!isBalanced(root.left) || !isBalanced(root.right)){
+            return false;
+        }
+
+        if(Math.abs(maxDepth(root.left)-maxDepth(root.right)) > 1){
+            return false;
+        }
+
+        return true;
+    }
+
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
     }
 }
