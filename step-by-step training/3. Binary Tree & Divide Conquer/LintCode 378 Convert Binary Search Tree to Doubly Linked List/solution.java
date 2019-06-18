161c8f395d464f448aa74884ebbfb87119a8c616
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-18 11:29:29 -0500

    Create solution.java

---
+++ step-by-step training/3. Binary Tree & Divide Conquer/LintCode 378 Convert Binary Search Tree to Doubly Linked List/solution.java
@@ -0,0 +1,42 @@
+public class Solution{
+    Class ResultType{
+        DoublyListNode head;
+        DoublyListNode tail;
+        Public ResultType( DoublyListNode head, DoublyListNode tail){
+            this head = head;
+            this.tail = tail;
+            }
+     }
+     
+     
+     Public DoublyListNode bstToDoublyList(TreeNode root){
+          ResultType result = helper(root);
+          return result.head;
+     }
+     
+     Public ResultType helper(TreeNode root){
+         if(root == null){
+             return new ResultType(null,null);
+             }
+         
+         ResultType left = helper(root.left);
+         ResultType right = helper(root.right);
+         
+         ResultType curr = new ResultType(null,null);
+         DoublyListNode node = new DoublyListNode(root.val);
+         
+         if(left.head != null){
+             left.tail.next = node;
+             node.prev = left.tail;
+             curr.head = left.head;
+             }
+             
+         else{
+             curr.tail = node;
+             }
+         
+         return curr;
+         }
+         
+         
+    }
