36345ca75999f96b123b0b8ff9ff6ecf94a547f0
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-18 14:30:33 -0500

    Update solution.java

--- step-by-step training/3. Binary Tree & Divide Conquer/LeetColde 98. Validate Binary Search Tree/solution.java
+++ step-by-step training/3. Binary Tree & Divide Conquer/LeetColde 98. Validate Binary Search Tree/solution.java
@@ -1,3 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/***
+//recursion
+//time: O(n) visit every node once
+//space:O(n) keep the entire tree
 class Solution {
     public boolean helper(TreeNode node, Integer lower, Integer upper){
         if(node == null) return true;
@@ -12,5 +25,41 @@ class Solution {
     }
     public boolean isValidBST(TreeNode root) {
         return helper(root,null,null);
+    }
+}
+*/
+
+//iteration
+//stack + dfs
+class Solution{
+    LinkedList<TreeNode> stack = new LinkedList();
+    LinkedList<Integer> uppers = new LinkedList(), lowers = new LinkedList();
+    
+    public void update(TreeNode root, Integer lower, Integer upper){
+        stack.add(root);
+        lowers.add(lower);
+        uppers.add(upper);
+    }
+    
+    
+    public boolean isValidBST(TreeNode root){
+        Integer lower = null, upper = null, val;
+        update(root,lower,upper);
+        
+        while(!stack.isEmpty()){
+            root = stack.poll();
+            lower = lowers.poll();
+            upper = uppers.poll();
+            
+            if(root == null) continue;
+            val = root.val;
+            if(lower != null && val <= lower) return false;
+            if(upper != null && val >= upper) return false;
+            
+            update(root.right, val, upper);
+            update(root.left, lower, val);
+            
+        }
+        return true;
     }
 }
