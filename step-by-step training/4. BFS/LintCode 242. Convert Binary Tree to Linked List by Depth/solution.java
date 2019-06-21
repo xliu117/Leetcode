c0934242c3b3c8d96fc9bbffaacc1b1083ce00f4
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-21 10:12:49 -0500

    Update solution.java

--- step-by-step training/4. BFS/LintCode 242. Convert Binary Tree to Linked List by Depth/solution.java
+++ step-by-step training/4. BFS/LintCode 242. Convert Binary Tree to Linked List by Depth/solution.java
@@ -1,2 +1,38 @@
 //二叉树的层次遍历，每层的数据用链表存储
 
+public class Solution{
+  public List<ListNode> binaryTreeToLists(TreeNode root){
+    List<ListNode> result = new ArrayList<>();
+    if(root == null) return result;
+    
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    
+    while(!queue.isEmpty()){
+      ListNode head = null;
+      ListNode pointer = null; //
+      int size = queue.size();
+      
+      for(int i = 0; i < size; i++){
+        TreeNode node = queue.poll();
+        if(i == 0){
+          head = new ListNode(node.val);
+          pointer = head;
+        }
+        else{
+          pointer.next = new ListNode(node.val);
+          pointer = pointer.next;
+        }
+        
+        if(node.left != null){
+          queue.offer(node.left);
+        }
+        if(node.right != null){
+          queue.offer(node.right);
+        }
+      }
+      result.add(head);
+    }
+    return result;
+  }
+}
