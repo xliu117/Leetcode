5aef7d14ae054b5738cc5fdc97d1d0312019f5c5
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-24 16:16:08 -0500

    Update solution.java

--- step-by-step training/4. BFS/LintCode 431.Connected component in undirected graph/solution.java
+++ step-by-step training/4. BFS/LintCode 431.Connected component in undirected graph/solution.java
@@ -1,1 +1,13 @@
 class Solution{
+
+  
+  public class UF{
+      public HashMap<Integer, Integer> root;
+      root = new HashMap<>();
+      for(Integer nodeLabel : hash){
+           root.put(nodeLabel, nodeLabel);
+      }
+  }
+  
+  public int find(int nodeLabel){
+      while(nodeLabel
