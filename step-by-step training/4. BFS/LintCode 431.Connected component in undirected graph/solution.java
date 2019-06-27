221323876dca01fda0d9d86464da0f9c2ad03d17
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-27 14:16:42 -0500

    Update solution.java

--- step-by-step training/4. BFS/LintCode 431.Connected component in undirected graph/solution.java
+++ step-by-step training/4. BFS/LintCode 431.Connected component in undirected graph/solution.java
@@ -1,13 +1,26 @@
-class Solution{
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] roots = new int[n];
+        for(int i = 0; i < n; i++) roots[i] = i;
+        
+        for(int[] e : edges){
+            int root1 = find(roots,e[0]);
+            int root2 = find(roots,e[1]);
+            if(root1 != root2){//union
+                roots[root1] = root2;
+                n--;
+            }
+        }
+        return n;
+    }
+    
+    public int find(int[] roots, int id){
+        while(roots[id] != id){
+            roots[id] = roots[roots[id]];//path compression
+            id = roots[id];
+        }
+        return id;
+    }
+}
 
-  
-  public class UF{
-      public HashMap<Integer, Integer> root;
-      root = new HashMap<>();
-      for(Integer nodeLabel : hash){
-           root.put(nodeLabel, nodeLabel);
-      }
-  }
-  
-  public int find(int nodeLabel){
-      while(nodeLabel
+
