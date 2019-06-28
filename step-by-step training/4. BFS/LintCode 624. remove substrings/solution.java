2edc893863b3e8be5054edaa309bf1b00ca30faa
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-06-28 17:21:38 -0500

    Create solution.java

---
+++ step-by-step training/4. BFS/LintCode 624. remove substrings/solution.java
@@ -0,0 +1,30 @@
+class Solution{
+    public int minLength(String s, Set<String> dict){
+         Queue<String> que = new LinkedList<String>();
+         Set<String> hash = new HashSet<String>();
+         
+         int min = s.length();
+         que.offer(s);
+         hash.add(s);
+         
+         while(!que.isEmpty()){
+             s = que.poll();
+             for(String sub : dict){
+                int found = s.indexOf(sub);
+                while(found != -1){
+                    String new_s = s.substring(0,found) + s.substring(found+sub.lenght(),s.length());
+                    if(!hash.contains(new_s){
+                       if(new_s.length() < min){
+                            min = new_s.length();
+                            }
+                       que.offer(new_s);
+                       hash.add(new_s);
+                    }
+                    found = s.indexOf(sub,found+1);
+                }
+             }
+         }
+         return min;
+      }
+   }
+   
