06e531c5b5e0e9b83112bf18d499160ac377deb3
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-07-22 16:41:18 -0500

    Create solution.java

---
+++ step-by-step training/7. Two Pointers/LintCode 8. Rotate String/solution.java
@@ -0,0 +1,19 @@
+class Solution{
+    public void rotateString(char[] str, int offset){
+         if(str == null || str.length == 0)
+              return;
+              
+         offset = offset % str.length;
+         reverse(str, 0, str.length - offset - 1);
+         reverse(str, str.length - offset, str.length - 1);
+         reverse(str,0,str.length - 1);
+         }
+         
+    private void reverse(char[] str, int start, int end){
+        for(int i = start, j = end; i < j; i++, j--){
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            }
+         }
+     }    
