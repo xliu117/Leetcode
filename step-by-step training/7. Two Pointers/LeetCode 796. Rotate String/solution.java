f0bc26c108d0ffea15ea00c6c9cb6b4f3405b064
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-07-22 16:47:11 -0500

    Update solution.java

--- step-by-step training/7. Two Pointers/LeetCode 796. Rotate String/solution.java
+++ step-by-step training/7. Two Pointers/LeetCode 796. Rotate String/solution.java
@@ -8,6 +8,7 @@ class Solution {
 
 ///KMP
 //KMP (Knuth-Morris-Pratt) 
+/***
 Intuition
 
 As before, we want to find whether B exists in A+A. The KMP algorithm is a textbook algorithm that does string matching in linear time, which is faster than brute force.
@@ -31,3 +32,33 @@ Now when trying to find the largest length matching for (A+A) at position i, it 
 Again, our strategy is to find a matching (match_len2 - 1, i - 1) plus check that B[match_len2] == (A+A)[i]. Similar to before, if B[match_len] != (A+A)[i], then because (match_len - 1, i - 1) was a matching, by transitivity (match_len2 - 1, match_len - 1) must be a matching, of which the largest is found by match_len2 = match_len - shifts[match_len]. We also repeatedly check these match_len's in order from largest to smallest.
 
 If at any point in this algorithm our match length is N, we've found B in A+A successfully.
+*/
+    
+    
+    class Solution {
+    public boolean rotateString(String A, String B) {
+        int N = A.length();
+        if (N != B.length()) return false;
+        if (N == 0) return true;
+
+        //Compute shift table
+        int[] shifts = new int[N+1];
+        Arrays.fill(shifts, 1);
+        int left = -1;
+        for (int right = 0; right < N; ++right) {
+            while (left >= 0 && (B.charAt(left) != B.charAt(right)))
+                left -= shifts[left];
+            shifts[right + 1] = right - left++;
+        }
+
+        //Find match of B in A+A
+        int matchLen = 0;
+        for (char c: (A+A).toCharArray()) {
+            while (matchLen >= 0 && B.charAt(matchLen) != c)
+                matchLen -= shifts[matchLen];
+            if (++matchLen == N) return true;
+        }
+
+        return false;
+    }
+}
