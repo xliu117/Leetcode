3267124e5bbf84982dab6854842386286b4bc9a7
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-07-23 20:44:50 -0500

    Update solution.java

--- step-by-step training/7. Two Pointers/LIntCode 610 Two Sum - Difference equals to target/solution.java
+++ step-by-step training/7. Two Pointers/LIntCode 610 Two Sum - Difference equals to target/solution.java
@@ -45,5 +45,32 @@ class Solution{
              }
           }   
               
-          
+//solution2: use hashmap        
+class Solution{
+public int[] twoSum7(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int sum = nums[i] + target;
+        if (map.containsKey(sum)) {
+            int index = map.get(sum);
+            int[] pair = new int[2];
+            pair[0] = index + 1;
+            pair[1] = i + 1;
+            return pair;
+        }
+        
+        int diff = nums[i] - target;
+        if (map.containsKey(diff)) {
+            int index = map.get(diff);
+            int[] pair = new int[2];
+            pair[0] = index + 1;
+            pair[1] = i + 1;
+            return pair;
+        }
+        map.put(nums[i], i);
+    }
+    
+    return null;
+}
+
          
