a4c6be5fef2239f2f7ae355f172b8eaeca360f6a
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-07-25 17:31:22 -0500

    Update solution.java

--- step-by-step training/7. Two Pointers/LintCode 461 Kth Smallest Numbers in Unsorted Array/solution.java
+++ step-by-step training/7. Two Pointers/LintCode 461 Kth Smallest Numbers in Unsorted Array/solution.java
@@ -1,11 +1,43 @@
-class Solution{
-   public int kthSmallest(int k, int[] nums){
-      return helper(nums, 0, nums.length - 1, k - 1);
-   }
-   
-   public int helper(int[] nums, int lo, int hi, int k){
-       if(hi <= lo){
-          return nums[hi];
-       }
-       
-       if(pos
+
+class Solution {
+    /*
+     * @param k an integer
+     * @param nums an integer array
+     * @return kth smallest element
+     */
+    public int kthSmallest(int k, int[] nums) {
+        return quickSelect(nums, 0, nums.length - 1, k - 1);
+    }
+
+    public int quickSelect(int[] A, int start, int end , int k) {
+        if (start == end)
+            return A[start];
+        
+        int left = start, right = end;
+        int pivot = A[(start + end) / 2];
+
+        while (left <= right) {
+            while (left <= right && A[left] < pivot) {
+                left++;
+            }
+            while (left <= right && A[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                
+                left++;
+                right--;
+            }
+        }
+
+        if (right >= k && start <= right)
+            return quickSelect(A, start, right, k);
+        else if (left <= k && left <= end)
+            return quickSelect(A, left, end, k);
+        else
+            return A[k];
+    }
+}
