bf5d17b9d86490159c787de7fb42da9e9f6f9893
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-07-26 14:19:17 -0500

    Update solution.java

--- step-by-step training/7. Two Pointers/LeetCode 75. Sort Colors/solution.java
+++ step-by-step training/7. Two Pointers/LeetCode 75. Sort Colors/solution.java
@@ -1,23 +1,31 @@
+//Let's use here three pointers to track the rightmost boundary of zeros, the leftmost boundary of twos and the current element under the consideration.
+
+
+//The idea of solution is to move curr pointer along the array, if nums[curr] = 0 - swap it with nums[p0], if nums[curr] = 2 - swap it with nums[p2].
+
 class Solution {
     public void sortColors(int[] nums) {
         if(nums.length < 1 || nums == null){
             return;
         }
         
-        int left = 0;
-        int right = nums.length - 1;
-        int v = 1;
-        int i = 0;
+        int left = 0; // during the algorithm execution nums[idx < left] = 0;
+        int right = nums.length - 1; // during the algorithm excecution nums[idx > right] = 0;
+  
+        int currIndex = 0;
         
-        while(i <= right){
-           if(nums[i] < v){
-               swap(nums,left++,i++);
+        while(currIndex <= right){
+            //if nums[currIndex] = 0, swap curr-th and left-th element and move both pointers to the right;
+           if(nums[currIndex] ==0){
+               swap(nums,left++,currIndex++);
            }
-            else if(nums[i] > v){
-                swap(nums,i,right--);
+            //if(nums[currIndex] = 2), swap curr-th and right-th element. Move right pointer the the left;
+            else if(nums[currIndex] ==2 ){
+                swap(nums,currIndex,right--);
             }
+            //if(nums[currIndex] == 1), move pointer currIndex to the right.
             else{
-                i++;
+                currIndex++;
             }
         }
     }
