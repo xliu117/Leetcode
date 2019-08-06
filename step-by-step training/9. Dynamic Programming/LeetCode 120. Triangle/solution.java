6ed298984c9d6159577a17c2ca785f65b44f78c8
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-08-06 11:32:55 -0500

    Create solution.java

---
+++ step-by-step training/9. Dynamic Programming/LeetCode 120. Triangle/solution.java
@@ -0,0 +1,34 @@
+public class Solution{
+    public int minimumTotal(int[][] triangle){
+        if(triangle == null || triangle.length == 0 || tirangle[0] == null || tirangle[0].length == 0){
+            return 0;
+        }
+        
+        int m = triangle.length;
+        int[][] f = new int[m][m];
+        
+        f[0][0] = triangle[0][0];
+        for(int i = 1; i < m; i++){
+           f[i][0] = f[i-1][0] + triangle[i][0];
+           f[i][i] = f[i-1][i-1] + triangle[i][i];
+        }
+        
+        for(int i = 2; i < m; i++){
+            for(int j = 1; j < i; j++){
+               f[i][j] = Math.min(f[i-1][j-1], f[i-1][j]) +  triangle[i][j];
+            }
+         }
+         
+         int min = Integer.MAX_VALUE;
+         for(int i = 0; i < m; i++){
+             if(min > f[m-1][i]){
+                min = f[m-1][i];
+             }
+         }
+         
+         return min;
+      }
+      
+  }    
+         
+         
