d71b19609a146d2ad6f00b250ea2b49cf720eed2
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-08-06 20:39:13 -0500

    Update solution.java

--- step-by-step training/9. Dynamic Programming/LeetCode 62. Unique Paths/solution.java
+++ step-by-step training/9. Dynamic Programming/LeetCode 62. Unique Paths/solution.java
@@ -1,3 +1,5 @@
+/***
+//my solution
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
@@ -18,4 +20,21 @@ class Solution {
         }
         return dp[0][0];
     }
+}
+*/
+
+
+public class Solution {
+public int uniquePaths(int m, int n) {
+    int[][] grid = new int[m][n];
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            if(i==0||j==0)
+                grid[i][j] = 1;
+            else
+                grid[i][j] = grid[i][j-1] + grid[i-1][j];
+        }
+    }
+    return grid[m-1][n-1];
+}
 }
