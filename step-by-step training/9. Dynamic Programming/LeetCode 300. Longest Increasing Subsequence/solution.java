8aa710903f5eb7c4871f530366ca15ad67d37210
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-08-15 16:19:38 -0500

    Update solution.java

--- step-by-step training/9. Dynamic Programming/LeetCode 300. Longest Increasing Subsequence/solution.java
+++ step-by-step training/9. Dynamic Programming/LeetCode 300. Longest Increasing Subsequence/solution.java
@@ -1,8 +1,10 @@
-
- 
+/***
+ //solution1:dp
+//time:O(n^2) there are two for loops traversing in the nums array.
+//space:O(n)  the int[n] dp array is used.
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        
+        //corner cases
         if(nums.length == 0 || nums == null){
             return 0;
         }
@@ -11,6 +13,7 @@ class Solution {
         }
         
         //considering dp
+        //dp[i] represents the length of the longest increasing subsequene possible considering the array elements upto the ith index.
         int[] dp = new int[nums.length];
         dp[0]= 1;
         int maxans = 1;
@@ -28,3 +31,33 @@ class Solution {
         return maxans;
     }
 }
+*/
+
+
+
+//2. DP with binary search
+class Solution{
+    public int lengthOfLIS(int[] nums){
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for(int x : nums){
+            int i = 0, j = size;
+            while(i != j){
+                int m = (i+j)/2;
+                if(tails[m] < x){
+                    i = m+1;
+                }else{
+                    j = m;
+                }
+            }
+            tails[i] = x;
+            if(i == size){
+                ++size;
+            }
+    }
+        return size;
+}
+}
+
+//time:O(nlogn)
+//space:O(n)
