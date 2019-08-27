e44fca06476de45466179af88d07acf70d0cef66
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-08-26 19:24:11 -0500

    Update solution.java

--- Facebook/LC173. Binary Search Tree Iterator/solution.java
+++ Facebook/LC173. Binary Search Tree Iterator/solution.java
@@ -47,3 +47,56 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+
+/* Solution2: Stack*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+    
+
+    public BSTIterator(TreeNode root) {
+        this.stack = new Stack<TreeNode>();
+        this._leftmostInorder(root);
+        
+    }
+    
+    
+    private void _leftmostInorder(TreeNode root){
+        while(root != null){
+            this.stack.push(root);
+            root =root.left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode topmostNode = this.stack.pop();
+        
+        if(topmostNode.right != null){
+            this._leftmostInorder(topmostNode.right);//find the next larger node， it will be always on the top of stack
+        }
+        
+        return topmostNode.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return this.stack.size() > 0;//when the stack is empty, there will not be next node
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
