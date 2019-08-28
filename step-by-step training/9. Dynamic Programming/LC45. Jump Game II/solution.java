0b3e1e2a8c13f875cf95c5fe8c3440b992964a63
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-08-28 14:25:17 -0500

    Update solution.java

--- step-by-step training/9. Dynamic Programming/LC45. Jump Game II/solution.java
+++ step-by-step training/9. Dynamic Programming/LC45. Jump Game II/solution.java
@@ -1,4 +1,4 @@
- 
+/***
 //BFS
 class Solution{
     public int jump(int[] nums){
@@ -21,5 +21,29 @@ class Solution{
             curMax = furthest;
         }
         return -1;
+    }
+}*/
+
+class Solution{
+    public int jump(int[] nums){
+        int[] dp = new int[nums.length];
+        dp[0]=0;
+        int curIndex = 0;
+        int curMax = 0;
+        int max = nums[0];//the furthest we can get right now.
+        
+        for(int i = 1; i < nums.length; i++){
+            curMax = Math.max(curMax, nums[i] + i);
+            
+            if(i<max){
+                dp[i] = dp[curIndex] + 1;
+            }else if (i == max){
+                dp[i] = dp[curIndex] + 1;
+                max = curMax;
+                curIndex = i;
+            }
+        }
+        
+        return dp[nums.length - 1];
     }
 }
