482224041d7f72ded99b68efbe1f070f4ac969b9
xliu117 <39917651+xliu117@users.noreply.github.com>
2019-08-28 14:39:53 -0500

    Update solution.java

--- step-by-step training/9. Dynamic Programming/LC45. Jump Game II/solution.java
+++ step-by-step training/9. Dynamic Programming/LC45. Jump Game II/solution.java
@@ -1,4 +1,4 @@
-/***
+ /***
 //BFS
 class Solution{
     public int jump(int[] nums){
@@ -26,18 +26,21 @@ class Solution{
 
 class Solution{
     public int jump(int[] nums){
-        int[] dp = new int[nums.length];
+        int[] dp = new int[nums.length];//dp[i] indicated the minimum step to get to i.
         dp[0]=0;
         int curIndex = 0;
         int curMax = 0;
         int max = nums[0];//the furthest we can get right now.
         
         for(int i = 1; i < nums.length; i++){
+            
+            //the furtest place we can get from the indexes we can get right now
             curMax = Math.max(curMax, nums[i] + i);
             
             if(i<max){
+                //inside the scope of index we can get, so the minimum step is dp[curIndex] +1
                 dp[i] = dp[curIndex] + 1;
-            }else if (i == max){
+            }else if (i == max){//if i is at the end of the scope right now, update the next max scope and the curIndex.
                 dp[i] = dp[curIndex] + 1;
                 max = curMax;
                 curIndex = i;
